3ce0365f367a73eaffe0e61c1dec5c9cbb73750d
Gustavo <carneirinho@gmail.com>
2020-02-03 13:31:14 -0300

    Added Proptypes

---
+++ src/components/TechItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function TechItem ({tech, onDelete}){
+    return (
+        <li key={tech}>{tech} <button type="button" onClick={onDelete}>remover</button></li>
+        
+    )
+}
+
+TechItem.defaultProps = {
+    tech: 'Oculto',
+}
+
+TechItem.propTypes = {
+    tech: PropTypes.string,
+    onDelete: PropTypes.func.isRequired
+}
+
+export default TechItem;
